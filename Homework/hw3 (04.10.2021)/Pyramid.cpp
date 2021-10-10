0c7648d21667ff62283ffc90b2eb7bc748cf36e7
danya <35501515+rism1@users.noreply.github.com>
2021-10-11 02:14:43 +0300

    Add hw3

---
+++ Homework/hw3 (04.10.2021)/Pyramid.cpp
@@ -0,0 +1,114 @@
+
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <string>
+using namespace std;
+typedef unsigned long long int ll;
+
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+
+	// Top part
+	for (int i = 0; i < n - 1; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			for (int a = n - 1 - i; a > 0; a--)
+			{
+				cout << " ";
+			}
+
+			for (int b = 0; b < i; b++)
+			{
+				cout << "*";
+			}
+			cout << "*";
+			for (int b = 0; b < i; b++)
+			{
+				cout << "*";
+			}
+			for (int a = n - 1 - i; a > 0; a--)
+			{
+				cout << " ";
+			}
+		}
+		cout << "\n";
+	}
+
+	//Middle part
+	for (int j = 0; j < m; j++)
+	{
+		for (int c = 0; c < n - 1; c++)
+		{
+			cout << "*";
+		}
+		cout << "*";
+		for (int c = 0; c < n - 1; c++)
+		{
+			cout << "*";
+		}
+	}
+	cout << "\n";
+
+	//Bottom part
+	for (int i = 0; i < n - 1; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			for (int a = 0; a < i + 1; a++)
+			{
+				cout << " ";
+			}
+			for (int b = 0; b < n - 2 - i; b++)
+			{
+				cout << "*";
+			}
+			cout << "*";
+			for (int b = 0; b < n - 2 - i; b++)
+			{
+				cout << "*";
+			}
+			for (int a = 0; a < i + 1; a++)
+			{
+				cout << " ";
+			}
+
+		}
+		cout << "\n";
+	}
+
+}
+//#include <iostream>
+//#include <cmath>
+//#include <algorithm>
+//#include <vector>
+//#include <string>
+//using namespace std;
+//
+//
+//int main() {
+//	int n;
+//	cin >> n;
+//	
+//	for (int i = 0; i < n; i++) {
+//		for (int j = 1; j < n - i; j++)
+//		{
+//			cout << ' ';
+//		}
+//		for (int j = 0; j <= i * 2; j++) {
+//			cout << "*";
+//		}
+//		for (int j = 1; j < n - i; j++)
+//		{
+//			cout << ' ';
+//		}
+//		cout << "\n";
+//	}
+//
+//
+//}
+
