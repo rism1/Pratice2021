44bc36c2530b48b977ba5ce75b4f71445b94d99a
danya <35501515+rism1@users.noreply.github.com>
2021-12-19 20:53:02 +0300

    Adjust program

--- Homework/hw (25.10.2021)/More Fridays.cpp
+++ Homework/hw (25.10.2021)/More Fridays.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-/* 
+/*
 
 Задание 2.
 
@@ -15,22 +15,22 @@ typedef long long int ll;
 int main()
 {
     int Year, Month, Day, a, y, m, d;
-    
-    ll count = 0;
+
+    ll month_count = (2000 - 1901 + 1) * 12;
     ll fri_count = 0;
-    
+
     for (int Year = 1901; Year <= 2000; Year++)
     {
-        for (int Month = 1; Month <= 12; Month++ )
+        for (int Month = 1; Month <= 12; Month++)
         {
-            for (int Day = 1; Day <= 31; Day++)
+            /*for (int Day = 1; Day <= 31; Day++)
             {
                 if ((Month == 2) && (Day == 30))
                 {
-                    if (!(((Year % 4 == 0) && (Year % 100 != 0)) || (Year % 400 == 0))) 
+                    if (!(((Year % 4 == 0) && (Year % 100 != 0)) || (Year % 400 == 0)))
                     {
                         break;
-                    }               
+                    }
                 }
 
                 if ((Month == 2) && (Day == 29))
@@ -53,12 +53,25 @@ int main()
                     ++fri_count;
                 }
                 ++count;
+            }*/
+
+            a = (14 - Month) / 12;
+            y = Year - a;
+            m = Month + 12 * a - 2;
+            d = (13 + y + (y / 4) - (y / 100) + (y / 400) + ((31 * m) / 12)) % 7;
+            if (d == 5)
+            {
+                ++fri_count;
             }
+            
         }
     }
-    cout << "Percentage of Fridays XX century: \t" << (float)fri_count / (float)count * 100.0f << "%\n";
+   /* cout << "Percentage of Fridays XX century: \t" << (float)fri_count / (float)count * 100.0f << "%\n";
     cout << "Number of Fridays in XX century: \t" << fri_count << "\n";
-    cout << "Number of days in XX century: \t\t" << count;
+    cout << "Number of days in XX century: \t\t" << count;*/
+    cout << "Percentage of Fridays the 13th per month in XX century: \t" << (float)fri_count / (float)month_count * 100.0f << "%\n";
+    cout << "Number of Fridays the 13th in XX century: \t" << fri_count << "\n";
+    cout << "Number of months in XX century: \t" << month_count << "\n";
 }
 
 
